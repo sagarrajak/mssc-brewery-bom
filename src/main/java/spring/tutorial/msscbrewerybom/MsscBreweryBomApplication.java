80840ed8902291dd24e519a41a0bd430cad7e462
sagar <sagarrajak858@gmail.com>
2022-10-25 09:15:31 +0530

    first commit

---
+++ src/main/java/spring/tutorial/msscbrewerybom/MsscBreweryBomApplication.java
@@ -0,0 +1,13 @@
+package spring.tutorial.msscbrewerybom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/tutorial/msscbrewerybom/MsscBreweryBomApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.tutorial.msscbrewerybom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
